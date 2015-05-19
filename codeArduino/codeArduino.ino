5e6a7717ac9db6b4519db0ac72723dd87c419483
riklugtenberg <riklugtenberg@hotmail.com>
2015-05-19 15:18:21 +0200

    plus een

--- codeArduino/codeArduino.ino
+++ codeArduino/codeArduino.ino
@@ -24,7 +24,7 @@ int lijnTeller()
   while (lijnSensorWaarde >= lijnSensorDetectie)
   {
     lijnenGeteld++;
-    Serial.println("plus een
+    Serial.println("plus een");
     Serial.println(lijnSensorWaarde);
     delay(1000);
   }
