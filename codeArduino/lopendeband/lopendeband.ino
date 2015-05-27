d65ef3bb99b01633269938557ab77e65b4c5ea79
riklugtenberg <riklugtenberg@hotmail.com>
2015-05-27 15:37:20 +0200

    Werkende code

--- codeArduino/lopendeband/lopendeband.ino
+++ codeArduino/lopendeband/lopendeband.ino
@@ -1,6 +1,6 @@
 int  updownSnelheid, updown, leftrightSnelheid, leftright, lijnSensorDefault;
 String currentTask;
-int lijnSensorDetectie = 700;
+int lijnSensorDetectie = 760;
 boolean goLeft;
 void setup()
 {
@@ -16,73 +16,42 @@ void setup()
 
 void vanBandEenAf()
 {
-  while(analogRead(0) <= lijnSensorDetectie)
+      if(analogRead(0) <= lijnSensorDetectie)
       {
-        //terwijl een pakket wordt gedetecteerdt niks doen.
-      }
       delay(1000);//genoeg delay zodat het artikel tijd heeft om van de band af te vallen
       digitalWrite(leftright, HIGH);
       analogWrite(leftrightSnelheid, 0);
+      leftOrRight();
+      }
+      
+      else if(analogRead(0) > lijnSensorDetectie)
+      {
+      digitalWrite(leftright, HIGH);
+      analogWrite(leftrightSnelheid,255);
+      
+      }
 }
 
 void leftOrRight()
 {
-  if(currentTask == "left")
-  //{
-    //digitalWrite(updown, HIGH);
-    //analogWrite(updownSnelheid, 255);
-  //}
-  //else if(currentTask == "right"){
-    digitalWrite(updown, LOW);
+  //if(currentTask == "left")
+  
+    digitalWrite(updown, HIGH);
     analogWrite(updownSnelheid, 255);
-  //}
+  
+//  else if(currentTask == "right"){
+//    digitalWrite(updown, LOW);
+//    analogWrite(updownSnelheid, 255);
+  
 }
 
-void nextTask()
-{
-    if (Serial.available() > 0)
-    {
-      String currentTask = Serial.readStringUntil('\n');
-    }
-}
+
 
 void loop()
 {
+    vanBandEenAf();
     
     Serial.println(analogRead(0));
-   digitalWrite (updown, LOW);
-   analogWrite(updownSnelheid, 255);
-    digitalWrite(updown, HIGH);
-    analogWrite(updownSnelheid,255 );
-    nextTask();
-    if(currentTask == "empty")//delay van een paar sceonden als een bin moet worden leeg gemaakt.
-    {
-      delay(10000);
-      currentTask = "";
-    }else if(currentTask == "left" || currentTask == "right"){
-  
-      digitalWrite (leftright, HIGH);
-      analogWrite(leftrightSnelheid, 255);
-      
-      if(analogRead(0) <= lijnSensorDetectie)
-      {
-        vanBandEenAf();
-        leftOrRight();
-        delay(5000);//delay om te wachten tot het artikel in het pakket zit  
-        currentTask = "";    
-      }
-    }else
-    {
-      if(analogRead(0) <= lijnSensorDetectie)
-      {
-        vanBandEenAf();
-        leftOrRight();
-        delay(5000);//delay om te wachten tot het artikel in het pakket zit  
-         
-      }
-      digitalWrite (leftright, HIGH);
-      analogWrite(leftrightSnelheid, 255);
-      
-    }
+    Serial.println(currentTask);
   
 }
