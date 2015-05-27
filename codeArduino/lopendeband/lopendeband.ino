d743e860d71cad3714bacf72661ab0ced9346077
wfikkert <wfikkert@gmail.com>
2015-05-27 19:12:15 +0200

    werkende bpp arduino

--- codeArduino/lopendeband/lopendeband.ino
+++ codeArduino/lopendeband/lopendeband.ino
@@ -5,7 +5,7 @@ boolean goLeft;
 void setup()
 {
   Serial.begin(9600);
-  
+
   updownSnelheid = 5; // omhoog / omlaag: motorsnelheid
   updown = 4; // omhoog / omlaag : motorrichting
   leftrightSnelheid = 6; // links / rechts : motorsnelheid
@@ -16,46 +16,52 @@ void setup()
 
 void vanBandEenAf()
 {
-      if(analogRead(0) <= lijnSensorDetectie)
-      {
-      delay(1000);//genoeg delay zodat het artikel tijd heeft om van de band af te vallen
-      digitalWrite(leftright, HIGH);
-      analogWrite(leftrightSnelheid, 0);
-      Serial.println("Lijn gezien");
-      leftOrRight();
-      }
-      
-      else if(analogRead(0) > lijnSensorDetectie)
-      {
-      digitalWrite(leftright, HIGH);
-      analogWrite(leftrightSnelheid,255);
-      
-      }
+  delay(2000);//genoeg delay zodat het artikel tijd heeft om van de band af te vallen
+  digitalWrite(leftright, HIGH);
+  analogWrite(leftrightSnelheid, 0);
+  leftOrRight();
+
 }
 
 void leftOrRight()
 {
-  //if(currentTask == "left")
-  
+  if (currentTask == "left") {
+
     digitalWrite(updown, HIGH);
     analogWrite(updownSnelheid, 255);
-  
-//  else if(currentTask == "right"){
-//    digitalWrite(updown, LOW);
-//    analogWrite(updownSnelheid, 255);
-  
+  }
+  else if (currentTask == "right") {
+    digitalWrite(updown, LOW);
+    analogWrite(updownSnelheid, 255);
+  } else {
+    digitalWrite(updown, LOW);
+    analogWrite(updownSnelheid, 0);
+  }
+
 }
 
 
 
 void loop()
 {
-  if(Serial.available()>0){
-    currentTask =Serial.readStringUntil('\n');
-  }
-    vanBandEenAf();
-    
-//    Serial.println(analogRead(0));
-//    Serial.println(currentTask);
+  digitalWrite(leftright, HIGH);
+  analogWrite(leftrightSnelheid, 255);
   
+  while (analogRead(0) < lijnSensorDetectie)
+  {
+    // do nothing
+  }
+  Serial.println("lijn gedetecteerd");
+
+  while (Serial.available() < 0)
+  {
+    // do nothing
+  }
+
+  currentTask = Serial.readStringUntil('\n');
+  vanBandEenAf();
+
+  //    Serial.println(analogRead(0));
+  //    Serial.println(currentTask);
+
 }
