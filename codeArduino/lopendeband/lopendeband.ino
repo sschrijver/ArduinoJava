44e6132ad2994f0f44f9d35d58d65801a272b974
PinkishYellow <vincentkloppenburg1996@live.nl>
2015-05-27 13:55:26 +0200

    lopende band na dinges

--- codeArduino/lopendeband/lopendeband.ino
+++ codeArduino/lopendeband/lopendeband.ino
@@ -28,41 +28,38 @@ void vanBandEenAf()
 void leftOrRight()
 {
   if(currentTask == "left")
-  {
-    digitalWrite(updown, HIGH);
-    analogWrite(updownSnelheid, 255);
-  }
-  else if(currentTask == "right"){
+  //{
+    //digitalWrite(updown, HIGH);
+    //analogWrite(updownSnelheid, 255);
+  //}
+  //else if(currentTask == "right"){
     digitalWrite(updown, LOW);
     analogWrite(updownSnelheid, 255);
-  }
+  //}
 }
 
 void nextTask()
 {
-    String recv = Serial.readStringUntil('\n');
-
-    int commaIndex = recv.lastIndexOf(',');
-    int tweedeCommaIndex = recv.lastIndexOf(',', commaIndex + 1);
-
-    String currentTask = recv.substring(0, commaIndex);
+    if (Serial.available() > 0)
+    {
+      String currentTask = Serial.readStringUntil('\n');
+    }
 }
 
 void loop()
-{if (Serial.available() > 0)
-  {
-
+{
     
     Serial.println(analogRead(0));
-//    digitalWrite (updown, LOW);
-//    analogWrite(updownSnelheid, 255);
+   digitalWrite (updown, LOW);
+   analogWrite(updownSnelheid, 255);
     digitalWrite(updown, HIGH);
-    analogWrite(updownSnelheid, 0);
+    analogWrite(updownSnelheid,255 );
     nextTask();
     if(currentTask == "empty")//delay van een paar sceonden als een bin moet worden leeg gemaakt.
     {
       delay(10000);
-    }else{
+      currentTask = "";
+    }else if(currentTask == "left" || currentTask == "right"){
   
       digitalWrite (leftright, HIGH);
       analogWrite(leftrightSnelheid, 255);
@@ -71,8 +68,21 @@ void loop()
       {
         vanBandEenAf();
         leftOrRight();
-        delay(5000);//delay om te wachten tot het artikel in het pakket zit      
+        delay(5000);//delay om te wachten tot het artikel in het pakket zit  
+        currentTask = "";    
       }
+    }else
+    {
+      if(analogRead(0) <= lijnSensorDetectie)
+      {
+        vanBandEenAf();
+        leftOrRight();
+        delay(5000);//delay om te wachten tot het artikel in het pakket zit  
+         
+      }
+      digitalWrite (leftright, HIGH);
+      analogWrite(leftrightSnelheid, 255);
+      
     }
-  }
+  
 }
